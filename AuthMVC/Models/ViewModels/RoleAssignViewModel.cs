75ed4b4597b6b3b43566d9a3ed0bdf86436220cd
Berkant Karaca <berkantkaracatr@gmail.com>
2026-03-22 14:15:38 +0300

    RoleManager Sınıfı İle Rol Yönetimi

---
+++ AuthMVC/Models/ViewModels/RoleAssignViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthMVC.Models.ViewModels;
+
+public class RoleAssignViewModel
+{
+    public int RoleId { get; set; }
+    public string RoleName { get; set; }
+    public bool HasAssign { get; set; }
+}
---
+++ AuthMVC/Models/ViewModels/RoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthMVC.Models.ViewModels;
+
+public class RoleViewModel
+{
+    [Required(ErrorMessage = "Rolü boş geçmeyiniz.")]
+    [Display(Name = "Rol Adı")]
+    public string Name { get; set; }
+}
